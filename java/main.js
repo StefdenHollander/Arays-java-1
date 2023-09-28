3861ea09937aeb6f910353452902a89fed1cd21c
S den Hollander <302045198@student.rocmondriaan.nl>
2023-09-28 12:53:42 +0200

    update

---
+++ java/main.js
@@ -0,0 +1,44 @@
+console.log('js running....')
+
+//opdracht 1
+const array = [4, 5, 8, 12, -1, 99]
+
+console.log(array);
+
+//opdracht 2
+const array2 = [0.33, 4.8, 11.1, 34.5, 129.1, -2.56, -17.4]
+
+//opdracht 3
+const array3 = ['rood', 'groen', 'geel', 'blauw', 'roze']
+
+console.log(array3)
+
+//opdracht 4
+array[3] = 21;
+console.log(array);
+
+//opdracht 5
+array2[0] = 3.3;
+array2[6] = -1.74;
+console.log(array2);
+
+//opdracht 6 
+array3[3] = 'paars'
+
+console.log(array3)
+//opdracht 7
+array3.push('zwart');
+
+
+//opdracht 8
+array.unshift(0)
+
+console.log(array)
+
+//opdracht 9
+array2.pop(6)
+array2.shift(0)
+
+console.log(array2)
+
+//opdracht 10
